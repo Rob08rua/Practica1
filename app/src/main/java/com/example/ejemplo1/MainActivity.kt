e3a8c9d0599fbb3ab2ce43fc4162b0cab9c187e9
Practica2 <rcarlos_ruamtz.08@outlook.com>
2025-02-27 18:00:18 -0600

    Sexto programa image

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,49 +4,113 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Greeting(name = "Android")
-                    }
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        //fontSize = 100.sp,
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Ejemplo1Theme {
-        Column {
-            Greeting(name = "Roberto Carlos R.")
-            Greeting(name = "Irvin")
-        }
+    Column {
+        //SimpleImage()
+        //CircleImageView()
+        //RoundCornerImageView()
+        //ImageWithBackgroundColor()
+        ImageWithTint()
     }
+}
+
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "Andy Rubin",
+        //alignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+
+    )
+}
+
+@Composable
+fun CircleImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        alignment = Alignment.Center,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape)
+            .border(5.dp, androidx.compose.ui.graphics.Color.Gray , CircleShape)
+    )
+}
+
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, androidx.compose.ui.graphics.Color.Gray, RoundedCornerShape(10))
+    )
+}
+
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+        contentDescription = "",
+        modifier = Modifier
+            .size( 200.dp)
+            .background(androidx.compose.ui.graphics.Color.Red)
+            .padding(20.dp)
+    )
+}
+
+@Composable
+fun ImageWithTint() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size( 200.dp)
+    )
 }
